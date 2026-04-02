2d9173b81a0c637255ded93d16bf8c4d9679cc13
sudo14121 <9112554233n@gmail.com>
2026-04-02 13:49:25 +0300

    таблички

--- docx/fenic_docx/docs/update.py
+++ docx/fenic_docx/docs/update.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 sheets = {
-    "components_elitan.md": "https://docs.google.com/spreadsheets/d/1ndCj3_NiKzpPOuhefEcadm_SQtO8BG06wTF4i4alSe8/gviz/tq?tqx=out:csv&gid=1323661768",
-    "components_other.md": "https://docs.google.com/spreadsheets/d/1ndCj3_NiKzpPOuhefEcadm_SQtO8BG06wTF4i4alSe8/gviz/tq?tqx=out:csv&gid=76825789"
+    "components_elitan.md": "https://docs.google.com/spreadsheets/d/1ndCj3_NiKzpPOuhefEcadm_SQtO8BG06wTF4i4alSe8/export?format=csv&gid=1323661768",
+    "components_other.md": "https://docs.google.com/spreadsheets/d/1ndCj3_NiKzpPOuhefEcadm_SQtO8BG06wTF4i4alSe8/export?format=csv&gid=76825789"
 }
 
 for filename, url in sheets.items():
-    df = pd.read_csv(url)
-
-    df = df.fillna("")
-
-    with open(f"docx/fenic_docx/docs/{filename}", "w", encoding="utf-8") as f:
+    df = pd.read_csv(url).fillna("")
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(df.to_markdown(index=False))
+
+print("Готово")
+input("Нажми Enter чтобы выйти") 
